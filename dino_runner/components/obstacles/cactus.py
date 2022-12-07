5cea23855f932a1fa9e19c8395f133664dea0a57
R13Axis <rafael-alam@hotmail.com>
2022-12-07 11:39:05 -0600

    Tarea cactus large

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -7,4 +7,9 @@ class Cactus(Obstacles):
         super().__init__(image, self.type)
         self.rect.y = 325
 
+class Large(Obstacles):
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 325
         
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,14 +1,21 @@
+
+import random
 import pygame
-from dino_runner.utils.constants import SMALL_CACTUS
-from dino_runner.components.obstacles.cactus import Cactus
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD
+from dino_runner.components.obstacles.cactus import Cactus, Large
 
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
 
     def update(self, game):
+        turn = random.randint(0, 2)
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
+            if turn == 1:
+                self.obstacles.append(Cactus(SMALL_CACTUS))
+            else:
+                self.obstacles.append(Large(LARGE_CACTUS))
+            
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
